438de093ce7cb7be2397abb584e0e9fb997ef3c6
Austin Laturno <alaturno@gmail.com>
2022-05-28 07:43:52 -0700

    working on day 3. Pnly returning zeros

--- day3/day3.go
+++ day3/day3.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-
+    "os"
+    "bufio"
+//    "strconv"
+    "fmt"
 )
 
 func main() {
+    input, err :=  os.Open("input")
+    if err != nil {
+        fmt.Printf("Error opening file: %v\n", err)
+    }
+    defer input.Close()
+    scanner := bufio.NewScanner(input)
 
+    var gamma string
+    var epsilon string
+    for i := range [12]int{} {
+        one := 0
+        zero := 0
+        for scanner.Scan() {
+            line := scanner.Text()
+            if string(line[i]) == "0" {
+                zero++
+            } else {
+                one ++
+            }
+        }
+        if one > zero {
+            gamma += "1"
+            epsilon += "0"
+        } else {
+            gamma += "0"
+            epsilon += "1"
+        }
+    }
+    fmt.Println(gamma)
 }
