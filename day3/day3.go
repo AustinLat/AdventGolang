9416960508ea5ab5d69d1ea385a41c13ca7e9286
Austin Laturno <alaturno@gmail.com>
2022-08-06 13:14:02 -0700

    updating day 3

--- day3/day3.go
+++ day3/day3.go
@@ -1,74 +1,77 @@
 package main
 
 import (
-    "os"
-    "bufio"
-    "strconv"
-    "fmt"
+  "os"
+  "bufio"
+  "strconv"
+  "fmt"
 )
 
 func main() {
-    input, err :=  os.Open("input")
-    if err != nil {
-        fmt.Printf("Error opening file: %v\n", err)
-    }
-    defer input.Close()
-    scanner := bufio.NewScanner(input)
-    list := []string{}
-    for scanner.Scan() {
-        line := scanner.Text()
-        list = append(list, line)
-    }
-    fmt.Println(powerConsumption(list))
+  input, err :=  os.Open("input")
+  if err != nil {
+    fmt.Printf("Error opening file: %v\n", err)
+  }
+  defer input.Close()
+  scanner := bufio.NewScanner(input)
+  list := []string{}
+  for scanner.Scan() {
+    line := scanner.Text()
+    list = append(list, line)
+  }
+  fmt.Println(powerConsumption(list))
 }
 
 
 func lifeSupport(data []string) int64 {
-    one := 0
-    zero := 0
-    for i, line := range data {
-        if string(line[i]) == "0" {
-            zero++
-        } else {
-            one++
-        }
+  one := 0
+  zero := 0
+  for i, line := range data {
+    if string(line[i]) == "0" {
+      zero++
+    } else {
+      one++
     }
-    for i, line := range data {
-        if one > zero {
-            if 
-}
-
+  }
+  for i, line := range data {
+    if one > zero {
+      if string(line[i]) == "0" {
+        zero++
+      } else {
+        one++
+      }
+    }
+  }
 
 func powerConsumption(data []string) int64 {
-    var gamma string
-    var epsilon string
-    for i, _ := range [12]int{} {
-        one := 0
-        zero := 0
-        for _, line := range data {
-            if string(line[i]) == "0" {
-                zero++
-            } else {
-                one++
-            }
-        }
-        if one > zero {
-            gamma += "1"
-            epsilon += "0"
-        } else {
-            gamma += "0"
-            epsilon += "1"
-        }
+  var gamma string
+  var epsilon string
+  for i, _ := range [12]int{} {
+    one := 0
+    zero := 0
+    for _, line := range data {
+      if string(line[i]) == "0" {
+        zero++
+      } else {
+        one++
+      }
     }
+    if one > zero {
+      gamma += "1"
+        epsilon += "0"
+    } else {
+      gamma += "0"
+      epsilon += "1"
+    }
+  }
 
-    gammaInt, err := strconv.ParseInt(gamma, 2, 32)
-        if err != nil {
-            fmt.Println(err)
-        }
-    epsilonInt, err := strconv.ParseInt(epsilon, 2, 32)
-        if err != nil {
-            fmt.Println(err)
-        }
-
-    return gammaInt * epsilonInt
+  gammaInt, err := strconv.ParseInt(gamma, 2, 32)
+    if err != nil {
+      fmt.Println(err)
+    }
+  epsilonInt, err := strconv.ParseInt(epsilon, 2, 32)
+  if err != nil {
+    fmt.Println(err)
+  }
+  return gammaInt * epsilonInt
 }
